2223b85ca7e61b25b8c30cf7b81deb2676bf4b6a
Allan Amaral <allanmaral@hotmail.com>
2019-10-19 10:17:42 -0300

    Base porject setup

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "object-curly-newline": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+  },
+};
---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+// eslint-no-use-before-define
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gympoint',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
+// eslint-no-use-before-define
