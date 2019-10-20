b83b0a209a227a1250ee7db2fc36cec3f19057e0
Allan Amaral <allanmaral@hotmail.com>
2019-10-20 00:24:55 -0300

    feat: Added authentication middleware
    Authentication middleware added using jwt

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,13 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentsController from './app/controllers/StudentsController';
 
+import AuthenticationMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 
+routes.use(AuthenticationMiddleware);
 // Authenticated routes
 routes.post('/students', StudentsController.store);
 routes.put('/students', StudentsController.update);
